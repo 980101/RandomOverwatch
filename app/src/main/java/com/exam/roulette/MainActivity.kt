3624d28ae87f7d8adc8a252bdd2320aa5d1a98c3
hj <hyejuggang@gmail.com>
2021-05-03 23:26:46 +0900

    광고 제거

--- app/src/main/java/com/exam/roulette/MainActivity.kt
+++ app/src/main/java/com/exam/roulette/MainActivity.kt
@@ -3,62 +3,15 @@ package com.exam.roulette
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG = "로드"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        MobileAds.initialize(this) {}
-
-        val adRequest = AdRequest.Builder().build()
-        //광고를 로드한다.
-        banner.loadAd(adRequest)
-
-        banner.adListener = object: AdListener() {
-            override fun onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
-                Log.d(TAG, "MainActivity - onAdLoaded() called")
-            }
-
-            override fun onAdFailedToLoad(adError : LoadAdError) {
-                // Code to be executed when an ad request fails.
-                Log.d(TAG, "MainActivity - onAdFailedToLoad() called")
-            }
-
-            override fun onAdOpened() {
-                // Code to be executed when an ad opens an overlay that
-                // covers the screen.
-                Log.d(TAG, "MainActivity - onAdOpened() called")
-            }
-
-            override fun onAdClicked() {
-                // Code to be executed when the user clicks on an ad.
-                Log.d(TAG, "MainActivity - onAdClicked() called")
-            }
-
-            override fun onAdLeftApplication() {
-                // Code to be executed when the user has left the app.
-                Log.d(TAG, "MainActivity - onAdLeftApplication() called")
-            }
-
-            override fun onAdClosed() {
-                // Code to be executed when the user is about to return
-                // to the app after tapping on an ad.
-                Log.d(TAG, "MainActivity - onAdClosed() called")
-            }
-        }
 
         var n:Int
 
