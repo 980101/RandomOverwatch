bf375c45949e8df5527dd3fe2fd7e9db85eed161
980101 <jm30806@naver.com>
2021-08-15 15:40:49 +0900

    포지션 버튼 기능 구현 in 기본화면

--- app/src/main/java/com/exam/roulette/MainActivity.java
+++ app/src/main/java/com/exam/roulette/MainActivity.java
@@ -5,23 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static int number = 0;
 
-    class BtnOnClickListener implements View.OnClickListener {
+    class ItemBtnOnClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
             TextView tvNum = findViewById(R.id.tv_num);
             int value = Integer.parseInt(tvNum.getText().toString());
+
             switch (view.getId()) {
                 case R.id.btn_up :
-                    tvNum.setText(String.valueOf(value + 1));
+                    tvNum.setText(String.valueOf(checkRange(value + 1)));
                     break;
                 case R.id.btn_down :
-                    tvNum.setText(String.valueOf(value - 1));
+                    tvNum.setText(String.valueOf(checkRange(value - 1)));
                     break;
             }
+        }
+    }
+
+    class PosBtnOnClickListener implements View.OnClickListener {
+
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btn_tang :
+                    number = 8;
+                    break;
+                case R.id.btn_deal :
+                    number = 16;
+                    break;
+                case R.id.btn_heal :
+                    number = 7;
+                    break;
+            }
+        }
+    }
+
+    private static int checkRange(int value) {
+        if (value < 1) {
+            return value = number;
+        } else if (value > number) {
+            return value = 1;
+        } else {
+            return value;
         }
     }
 
@@ -30,12 +61,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BtnOnClickListener onClickListener = new BtnOnClickListener();
+        ItemBtnOnClickListener onItemClickListener = new ItemBtnOnClickListener();
 
         Button btnUp = findViewById(R.id.btn_up);
-        btnUp.setOnClickListener(onClickListener);
+        btnUp.setOnClickListener(onItemClickListener);
         Button btnDown = findViewById(R.id.btn_down);
-        btnDown.setOnClickListener(onClickListener);
+        btnDown.setOnClickListener(onItemClickListener);
+
+        PosBtnOnClickListener onPosClickListener = new PosBtnOnClickListener();
+
+        RadioButton btnTang = findViewById(R.id.btn_tang);
+        btnTang.setOnClickListener(onPosClickListener);
+        RadioButton btnDeal = findViewById(R.id.btn_deal);
+        btnDeal.setOnClickListener(onPosClickListener);
+        RadioButton btnHeal = findViewById(R.id.btn_heal);
+        btnHeal.setOnClickListener(onPosClickListener);
     }
 
 }
