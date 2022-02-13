da5933a47a8476b4d655ae13c75e82a92035bfa1
980101 <jm30806@naver.com>
2022-02-13 18:29:22 +0900

    반응형 Splash 화면으로 수정

--- app/src/main/java/com/exam/roulette/SplashActivity.kt
+++ app/src/main/java/com/exam/roulette/SplashActivity.kt
@@ -1,19 +1,25 @@
 package com.exam.roulette
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
-    val SPLASH_VIEW_TIME: Long = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
             finish()
-        }, SPLASH_VIEW_TIME)
+        }, DURATION)
+    }
+
+    companion object {
+        private const val DURATION: Long = 2000
     }
 }
